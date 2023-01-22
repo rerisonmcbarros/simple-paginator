8c4ca713713bbf7f21f8bca05804c3fb0d4481b8
Rerison Barros <rerisonmcb@gmail.com>
2023-01-22 16:19:26 -0300

    feat: adição do método que recebe dados
     No ramo main
     Mudanças a serem submetidas:
    	modified:   Paginator1.php

--- Paginator1.php
+++ Paginator1.php
@@ -7,6 +7,7 @@ class Paginator{
 	private $limit;
 	private $ofsset;
 
+	private $data;
 
 	public function __construct(string $url, $limit, $offset = 0){
 
@@ -16,5 +17,32 @@ class Paginator{
 	}
 
 
+	public function setData(array $data){
+
+		foreach($data as $value){
+
+			$this->data[] = $value;
+		}
+
+	}
 
 }
+
+
+
+// TESTES //
+
+
+$pdo = new \PDO("mysql:host=localhost;port=3306;dbname=users", "root", "");
+
+$stmt = $pdo->prepare("select * from users");
+
+$stmt->execute();
+
+$result = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+
+$paginator = new Paginator("url", 10, 1);
+
+$paginator->setData($result);
+
+echo "<pre>", var_dump($paginator), "</pre>";
